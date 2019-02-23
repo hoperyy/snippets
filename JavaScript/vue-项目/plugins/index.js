9d1e96f154e68ae3ade2a236bf4ea40cc36abfb8
hoperyy <liuyuanyang@weidian.com>
2019-02-24 00:01:33 +0800

    update

--- JavaScript/vue-项目/plugins/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import toastPlugin from './toast/index';
-import ajaxPlugin from './ajax/index';
-import loadingPlugin from './loading/index';
-export default {
-    toastPlugin,
-    ajaxPlugin,
-    loadingPlugin
-}
